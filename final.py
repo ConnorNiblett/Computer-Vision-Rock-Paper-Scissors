74f7b13b1a540013b25824d9273062cc9acbcaa7
Connor Niblett <connorniblett@icloud.com>
2022-07-07 13:56:46 +0100

    finished

--- final.py
+++ final.py
@@ -1,3 +1,4 @@
+from ssl import get_protocol_name
 import cv2
 from keras.models import load_model
 import numpy as np
@@ -18,7 +19,10 @@ ComputerScore = 0
 global user_choice 
 global prediction_max
 global computer
+global get_prediction
+global get_winner
 user_choice = ''
+winner_message = ''
 
 message = ''
 while True:
@@ -28,62 +32,89 @@ while True:
     normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
     data[0]= normalized_image
     prediction = model.predict(data)
-    if not started:
+    if started == False:
         message = 'Press a to start'
     if cv2.waitKey(33) == ord('a'):
-        if not started:
-            counter = time.time()
             started = True
+            counter = time.time()
             countdown = True
+            get_winner = False
+
+    if countdown:
+        message = f'Show your hand in { 5- int(elapsed)} seconds'
 
     if started:
         print('elapsed = ', elapsed)
         print('counter = ', counter)
-        print('time.time() - counter = ', time.time() - counter)
-        elapsed = 5 - (time.time() - counter)
-        if elapsed <= -5:
-                countdown = False
+        print('time.time() - counter = ',(time.time() - counter))
+        elapsed = (time.time() - counter)
+        if elapsed >= 5 :
+            countdown = False
+            if get_winner == False:
                 prediction_max = np.argmax(prediction)
                 if prediction_max == [0]:
                     user_choice = 'Rock'
                 elif prediction_max == [1]:
                     user_choice = 'Paper'
                 elif prediction_max == [2]:
-                     user_choice = 'Scissors'
+                    user_choice = 'Scissors'
                 computer = random.choice(['Rock', 'Paper', 'Scissors'])
-                message = 'Press n to play the next round'
-                if cv2.waitKey(33) == ord('n'):
-                    started = False
-                    elapsed = 0
-        
-        if elapsed <=0:
-            message = f'You showed {user_choice}, and the computer showed {computer}. '
-            if prediction_max == [0]:
-                if computer == 'Rock':
-                    message += "It's a draw"
-                elif computer == 'Paper':
-                    message += 'Sorry, you lose'
-                elif computer == 'Scissors':
-                    message += 'Great, you win!'
-            elif prediction_max == [1]:
-                if computer == 'Rock':
-                    message += 'Great, you win'
-                elif computer == 'Paper':
-                    message += "It's a draw"
-                elif computer == 'Scissors':
-                    message += 'Sorry, you lose'
-            elif prediction_max == [2]:
-                if computer == 'Rock':
-                    message += 'Sorry, you lose'
-                elif computer == 'Paper':
-                    message += 'Great, you win!'
-                elif computer == 'Scissors':
-                    message += "It's a draw"
-            else:
-                continue
-        if countdown:
-            message = f'Show your hand in {int(elapsed)} seconds'
 
+                if prediction_max == [0]:
+                    if computer == 'Rock':
+                        message = "It's a draw"
+                    elif computer == 'Paper':
+                        message = 'Sorry, you lose'
+                    elif computer == 'Scissors':
+                        message = 'Great, you win!'
+                elif prediction_max == [1]:
+                    if computer == 'Rock':
+                        message = 'Great, you win!'
+                    elif computer == 'Paper':
+                        message = "It's a draw"
+                    elif computer == 'Scissors':
+                        message = 'Sorry, you lose'
+                elif prediction_max == [2]:
+                    if computer == 'Rock':
+                        message = 'Sorry, you lose'
+                    elif computer == 'Paper':
+                        message = 'Great, you win!'
+                    elif computer == 'Scissors':
+                        message = "It's a draw"
+
+                winner_message = f'You showed {user_choice}, and the computer showed {computer}. '
+    
+                if message == 'Great, you win!':
+                    UserScore += 1
+                    print(UserScore)
+                    get_winner = True
+                elif message == 'Sorry, you lose':
+                    ComputerScore += 1
+                    print(ComputerScore)
+                    get_winner = True
+                else:
+                    get_winner = True
+                    continue
+                
+            if UserScore == 3:
+                print("You win")
+                break
+            if ComputerScore == 3:
+                print("You Lose")
+                break
+
+            round_restart = 'Hold n to play the next round' 
+            cv2.putText(frame, round_restart, (30, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)    
+            cv2.putText(frame, winner_message, (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)      
+            if cv2.waitKey(50) == ord('n'):
+                started = False
+                elapsed = 0   
+
+    print(UserScore, ComputerScore)
+    print(UserScore, ComputerScore)
+    print(UserScore, ComputerScore)
+    print(UserScore, ComputerScore)
+      
     # cv2.putText(frame, message, (150, 150), fontFace=)
     cv2.putText(frame, message, (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
     cv2.imshow('frame', frame)
@@ -98,5 +129,7 @@ cap.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
 
+    
+    
     
     
